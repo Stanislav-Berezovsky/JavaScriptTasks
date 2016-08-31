4dad1b17d384f762882d092e8235d4b7f076b7b1
Stanislau_Berazouski <Stanislau_Berazouski@EPBYMINW3373>
2016-08-31 17:53:22 +0300

    write myJQUery

--- JavaScriptTasks/SomeTasksJs.js
+++ JavaScriptTasks/SomeTasksJs.js
@@ -1,19 +1,41 @@
-function _(str){	
-	var selector =  str[0];
-	var name = str.slice(1);
-	switch(selector){
-		case "#" : 
-			this.context = document.getElementById(name);
-		case "." :
-			this.context = document.getElementsByClassName(name);
-		default :
-			this.context = document.querySelectorAll(str);
-	}
-}
+(function(window) {
+    function JQuery(str) {
+    }
 
-_.prototype.addClass = function(str) {
-	for (var item in this.context){
-		item.classList.add(str);
-	}
-	return this;	
-};
+    var elements;
+    var setElements = function(str){
+    	elements = document.querySelectorAll(str);
+    }
+
+    JQuery.prototype.addClass = function(str) {
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.add(str);
+        }
+        return this;
+    };
+
+    JQuery.prototype.css = function(key, value) {
+        for (var i = 0; i < elements.length; i++) {
+            var arr = key.split("-");
+            for (var j = 1; j < arr.length; j++) {
+                arr[j] = arr[j].charAt(0).toUpperCase() + arr[j].slice(1);
+            }
+            key = arr.join('');
+            elements[i].style[key] = value;
+        }
+        return this;
+    };
+
+    var instance;
+
+    window._ = function(str) {
+        if (!instance){
+            instance = new JQuery(str);
+        }
+        setElements(str);
+        return instance;
+    }
+
+ 
+
+})(window);
